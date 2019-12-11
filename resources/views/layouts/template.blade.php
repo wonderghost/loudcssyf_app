4f290326d3cda021ef02b6e25898dc8a14153ce0
AlMak47 <almamy@smartechguinee.com>
2019-12-11 16:46:56 +0000

    update

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>{{config('app.name')}}-@yield('title')</title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<!-- UIkit CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
