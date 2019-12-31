3540bc7ccda9f84a29caa0b6688eee34be327116
AlMak47 <almamy@smartechguinee.com>
2019-12-31 05:13:34 +0000

    ok

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -10,9 +10,8 @@
 </style>
 
 <article>
-    <h1>We&rsquo;ll be back soon!</h1>
+    <h1>Maintence en cours ...</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
+        <p>&mdash; la Team</p>
     </div>
 </article>
